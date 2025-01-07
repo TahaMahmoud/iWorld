beaddc4ed56c99169398894505b5db89655080d0
Taha Mahmoud <taha.nagy06@gmail.com>
2025-01-07 17:56:20 +0200

    Create Xcode project.

---
+++ iWorld/iWorld/ContentView.swift
@@ -0,0 +1,19 @@
+//
+//  ContentView.swift
+//  iWorld
+//
+//  Created by Taha Mahmoud on 07/01/2025.
+//
+
+import SwiftData
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("ContentView")
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ iWorld/iWorld/iWorldApp.swift
@@ -0,0 +1,17 @@
+//
+//  iWorldApp.swift
+//  iWorld
+//
+//  Created by Taha Mahmoud on 07/01/2025.
+//
+
+import SwiftUI
+
+@main
+struct iWorldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
